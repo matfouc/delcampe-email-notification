c037b41dcf4df88d95d8e717a3428aab10c53926
Matthieu Foucher <45356815+matfouc@users.noreply.github.com>
2022-04-04 11:24:04 +0200

    - 1st draft on dev branch

---
+++ src/main/java/com/lafouche/delcampeEmailNotification/delcampeEmailNotification.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lafouche.delcampeEmailNotification;
+
+/**
+ *
+ * @author Matthieu
+ */
+public class delcampeEmailNotification {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+//        String s = "Hpgjsri";
+//        
+//        switch(s) {
+//            case "Hi" -> System.out.println("result 1");
+//            case "Bonjour" -> System.out.println("result 2");
+//            case "Hej" -> System.out.println("result 3");
+//            default -> System.out.println("zut");
+//        }
+    }
+}
